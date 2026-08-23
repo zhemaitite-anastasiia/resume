595106a3f601dfbad21ca6226ab4ec331c75e02c
Anastasiia <a.zhemaitite@gmail.com>
2026-08-23 11:09:26 -0500

    Mount the sticker note popup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { FloatingStickers } from '@/components/floating-stickers'
 import { ResumeDeck } from '@/components/resume-deck'
 import { ContactModal } from '@/components/contact-modal'
+import { StickerNote } from '@/components/sticker-note'
 
 export default function Page() {
   return (
@@ -9,6 +10,7 @@ export default function Page() {
       <FloatingStickers />
       <ResumeDeck />
       <ContactModal />
+      <StickerNote />
     </main>
   )
 }
